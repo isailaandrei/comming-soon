bad4ac0e558b78df6ccc299be5c95923d3e8ee90
isailaandreii <isailaandrei.i@gmail.com>
2020-04-17 18:41:57 +0100

    Add rcaasig to allowed hosts

--- RCA/settings.py
+++ RCA/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['RCA_DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'rcaasigurari.herokuapp.com']
 
 
 # Application definition
